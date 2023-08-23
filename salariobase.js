4ec88ccd208c823fafa77b56b22199c7be80b5c7
RaylanSA <santosavilar.raylan@gmail.com>
2023-08-22 21:15:52 -0400

    acréscimo

--- salariobase.js
+++ salariobase.js
@@ -1,13 +1,18 @@
 var salario = 0; 
+var salarioLiquido = 0; 
 var salarioBase = parseFloat(prompt("Quanto é seu salário base? ")); 
 var horaSemanalTrabalhada = parseFloat(prompt("Quantas horas semanais trabalha? "));
 var hora = salarioBase/160
 var horaExtra = hora*1.5;
 if(horaSemanalTrabalhada >= 40) {
     salario = salarioBase +(horaExtra * (horaSemanalTrabalhada-40)); 
-    alert("R$"+ salario.toFixed(2)); 
+    salarioLiquido = salario * 0.975; 
+    alert("Salário bruto é de R$"+ salario.toFixed(2)) ; 
+    alert(" Salário líquido é de R$" + salarioLiquido.toFixed(2)); 
 }
 else {
     salario= salarioBase - (horaSemanalTrabalhada*hora); 
-    alert("R$"+ salario.toFixed(2)); 
+    salarioLiquido = salario * 0.975;
+    alert("Salário bruto é de R$"+ salario.toFixed(2)); 
+    alert(" Salário líquido é de R$" + salarioLiquido.toFixed(2)); 
 }
