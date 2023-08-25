7c1be56c1d9e4d633549aa53f4904a1495cbdc13
RaylanSA <santosavilar.raylan@gmail.com>
2023-08-24 21:04:57 -0400

    código comentando

--- salariobase.js
+++ salariobase.js
@@ -2,11 +2,11 @@ var salario = 0;
 var salarioLiquido = 0; 
 var salarioBase = parseFloat(prompt("Quanto é seu salário base? ")); 
 var horaSemanalTrabalhada = parseFloat(prompt("Quantas horas semanais trabalha? "));
-var hora = salarioBase/160
+var hora = salarioBase/160 // 40h da carga trabalhada semanal * 4 semanas do mês aproximada 
 var horaExtra = hora*1.5;
 if(horaSemanalTrabalhada >= 40) {
     salario = salarioBase +(horaExtra * (horaSemanalTrabalhada-40)); 
-    salarioLiquido = salario * 0.975; 
+    salarioLiquido = salario * 0.975; // ISS calculado é 2,5% 
     alert("Salário bruto é de R$"+ salario.toFixed(2)) ; 
     alert(" Salário líquido é de R$" + salarioLiquido.toFixed(2)); 
 }
